3bcf35182a329592c933854ddf83c818745caa38
Milesnocte <chrisknxt@gmail.com>
2021-03-02 00:51:34 -0500

    test push

--- src/main/java/Main/JDA.java
+++ src/main/java/Main/JDA.java
@@ -34,6 +34,7 @@ public class JDA extends ListenerAdapter implements EventListener {
         //Print invite link to console
         System.out.println("https://discord.com/api/oauth2/authorize?client_id=" + Credentials.BOTID + "&permissions=268446736&scope=bot");
 
+        //Test connect to the database
         DataBase db = new DataBase();
         db.databaseCycle();
     }
