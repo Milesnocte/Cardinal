753c155d34c8d5eaaaea12f1586189bf31ae71fa
Milesnocte <themilesnocte@gmail.com>
2021-03-06 03:02:50 -0500

    Code cleanup

--- src/main/java/Main/ScheduledTask.java
+++ src/main/java/Main/ScheduledTask.java
@@ -3,8 +3,7 @@ package Main;
 import java.util.Calendar;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
+import net.dv8tion.jda.api.events.Event;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import java.util.concurrent.TimeUnit;
 import net.dv8tion.jda.api.events.ReadyEvent;
@@ -12,10 +11,9 @@ import org.jetbrains.annotations.NotNull;
 
 public class ScheduledTask extends ListenerAdapter {
 
-    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-
     @Override
     public void onReady(@NotNull ReadyEvent event) {
+
         Timer timer = new Timer();
         Calendar date = Calendar.getInstance();
         date.set(Calendar.HOUR, 7);
@@ -26,18 +24,20 @@ public class ScheduledTask extends ListenerAdapter {
                 new TimerTask() {
                     @Override
                     public void run() {
-
-                        event.getJDA().getGuildById("433825343485247499").getTextChannelsByName("vc-text", true).get(0).createCopy().queue();
-
-                        try { TimeUnit.SECONDS.sleep(1);
-                        } catch (InterruptedException e) { }
-
-                        event.getJDA().getGuildById("433825343485247499").getTextChannelsByName("vc-text", true).get(0).delete().queue();
-
+                        refreshVCText(event);
                     }
                 },
                 date.getTime(),
                 1000 * 60 * 60 * 24
         );
+    }
+
+    public void refreshVCText(Event event){
+        event.getJDA().getGuildById("433825343485247499").getTextChannelsByName("vc-text", true).get(0).createCopy().queue();
+
+        try { TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) { }
+
+        event.getJDA().getGuildById("433825343485247499").getTextChannelsByName("vc-text", true).get(0).delete().queue();
     }
 }
